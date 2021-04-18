68688e64e53be453659652cd1682eccd9721fe63
bw984 <46266238+bw984@users.noreply.github.com>
2021-04-18 13:29:46 -0500

    Changed threaded to True as default.

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from config import Config
 server = create_app(dash_debug=Config.dash_debug, dash_auto_reload=Config.dash_auto_reload)
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0', port=8000, debug=Config.flask_debug, threaded=False)
+    server.run(host='0.0.0.0', port=8000, debug=Config.flask_debug, threaded=True)
